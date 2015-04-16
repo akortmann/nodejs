2081ec8f5d2cfe1e804634805091a284329c73ba
akortmann <andreas.kortmann@4meter20.net>
2015-04-16 19:16:09 +0200

    initial php

--- killerapp/app/index.php
+++ killerapp/app/index.php
@@ -1,3 +1,18 @@
+<?php
+
+    if(isset($_POST['submit'])){
+
+        if(trim($_POST['name']) != "" && $_POST['penisSize'] != ""){
+            $msg = $_POST['name']." hat einen ".$_POST['penisSize']." Penis!";
+        }else{
+            $msg = "Pimmel.. gib was ein!";
+        }
+
+    }else{
+        $msg = "";
+    }
+
+    $content = '
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -5,13 +20,14 @@
     <title>KillerApp</title>
 </head>
 <body>
-<div ng-view></div>
-<script src="http://localhost:1337/socket.io/socket.io.js"></script>
-<script>
-    var socket = io.connect('http://localhost:1337');
-    //    socket.on('hello', function(data) {
-    //        console.log('Ohh '+data);
-    //    });
-</script>
+<div class="notice">'.$msg.'</div>
+<form action="index.php?submit=true" method="POST">
+    <input type="text" name="name" value="" placeholder="Name.."/>
+    <input type="text" name="penisSize" value="" placeholder="Penis-Size in cm .. e.g. 36"/>
+    <input type="submit" value="hau raus!" />
+</form>
 </body>
-</html>
+</html>';
+
+echo $content;
+die();
