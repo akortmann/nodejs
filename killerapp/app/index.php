a5ed4daaa08f94ee9a796251e6b7faadd090cdf3
akortmann <andreas.kortmann@4meter20.net>
2015-04-16 19:42:51 +0200

    implement curl

--- killerapp/app/index.php
+++ killerapp/app/index.php
@@ -1,13 +1,43 @@
 <?php
 
-    if(isset($_POST['submit'])){
 
-        if(trim($_POST['name']) != "" && $_POST['penisSize'] != ""){
-            $msg = $_POST['name']." hat einen ".$_POST['penisSize']." Penis!";
+    if($_POST['submit']){
+
+        if(trim($_POST['name']) != "" && trim($_POST['penisSize'] != "")){
+
+            $msg = '<h1>'.$_POST['name']." hat einen ".$_POST['penisSize']."cm langen Penis!</h1>";
+
+            //extract data from the post
+            extract($_POST);
+
+            //set POST variables
+            $url = 'http://akortmann.indermache.net/get-post.php';
+            $fields = array(
+                'name' => urlencode($name),
+                'penisSize' => urlencode($penisSize),
+            );
+
+            //url-ify the data for the POST
+            foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+            rtrim($fields_string, '&');
+
+            //open connection
+            $ch = curl_init();
+
+            //set the url, number of POST vars, POST data
+            curl_setopt($ch,CURLOPT_URL, $url);
+            curl_setopt($ch,CURLOPT_POST, count($fields));
+            curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+            //execute post
+            $result = curl_exec($ch);
+
+            //close connection
+            curl_close($ch);
+
         }else{
-            $msg = "Pimmel.. gib was ein!";
+            $msg = "<h1>Pimmel.. gib was ein!</h1>";
         }
-
     }else{
         $msg = "";
     }
@@ -18,13 +48,20 @@
 <head>
     <meta charset="UTF-8">
     <title>KillerApp</title>
+    <script src="http://akortmann.indermache.net:1337/socket.io/socket.io.js"></script>
+<script>
+    var socket = io.connect(\'http://akortmann.indermache.net:1337\');
+//    socket.on(\'hello\', function(data) {
+//        console.log(\'Ohh \'+data);
+//    });
+</script>
 </head>
 <body>
 <div class="notice">'.$msg.'</div>
 <form action="index.php?submit=true" method="POST">
     <input type="text" name="name" value="" placeholder="Name.."/>
     <input type="text" name="penisSize" value="" placeholder="Penis-Size in cm .. e.g. 36"/>
-    <input type="submit" value="hau raus!" />
+    <input type="submit" name="submit" value="hau raus!" />
 </form>
 </body>
 </html>';
